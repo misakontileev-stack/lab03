27030e31c1deaf4628f6e42a7c8fe8e8e2339430
misha <@gmail.com>
2026-03-03 14:44:45 +0300

    Добавлены комментарии.

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
 #include <string>
 
+//Основная функция
 int main() {
-    std::string name;
-    std::cout << "Введите свое имя: ";
-    std::cin >> name;
-    std::cout << "Hello world from " << name << std:: endl;
+    std::string name; //Создана строковая переменная
+    std::cout << "Введите свое имя: "; //Вывод запроса
+    std::cin >> name; //Ввод имени
+    std::cout << "Hello world from " << name << std:: endl; //Вывод конечного результата
     return 0;
 }
