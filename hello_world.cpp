51f30a1667af0ebc66923fbbcc3b29120d03431c
misha <@gmail.com>
2026-03-03 15:04:27 +0300

    Изменен стиль с помощью опции -style=Mozilla

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,11 +1,14 @@
 #include <iostream>
 #include <string>
 
-//Основная функция
-int main() {
-    std::string name; //Создана строковая переменная
-    std::cout << "Введите свое имя: "; //Вывод запроса
-    std::cin >> name; //Ввод имени
-    std::cout << "Hello world from " << name << std:: endl; //Вывод конечного результата
-    return 0;
+// Основная функция
+int
+main()
+{
+  std::string name; // Создана строковая переменная
+  std::cout << "Введите свое имя: "; // Вывод запроса
+  std::cin >> name;                  // Ввод имени
+  std::cout << "Hello world from " << name
+            << std::endl; // Вывод конечного результата
+  return 0;
 }
